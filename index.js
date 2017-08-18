79664077145212842f92b56145a1c9edca632edd
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-08-18 14:52:00 -0700

    added treehouse model

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("INDEX LOCKED AND LOADED");
-})
+});
+
+class ThreeRenderer {
+  constructor() {
+    this.scene, this.camera, this.renderer;
+    
+    this.scene = new THREE.Scene();
+    this.WIDTH = window.innerWidth;
+    this.HEIGHT = window.innerHeight;
+  }
+}
