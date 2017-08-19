21e5d851782801e69880215f9d71815230ff4af2
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-08-18 20:37:35 -0700

    ThreeJS fisheye effect

--- js/index.js
+++ js/index.js
@@ -3,7 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("INDEX LOCKED AND LOADED");
   
   // Create the render window here
-  const threes = new ThreeRenderer();
+  // Hey Cyril, to increase the fisheye effect:
+  // (1) Go to this demo page: http://www.decarpentier.nl/downloads/lensdistortion-webgl/lensdistortion-webgl.html
+  // (2) Plug the numbers in order: horizontalFOV, strength, cylindricalRatio
+  const threes = new ThreeRenderer(140, 1, 0.25);
   
   window.addEventListener('resize', function() { 
     threes.resize(window.innerWidth, window.innerHeight)
@@ -13,11 +16,11 @@ document.addEventListener("DOMContentLoaded", function() {
   threes.animate();
 });
 
-
 // This class is responsible for rendering everything
+// FishEYE Source: https://stackoverflow.com/questions/13360625/
 class ThreeRenderer {
-  constructor() {
-    // bind animate so that it can call itself
+  constructor(horizontalFOV, strength, cylindricalRatio) {
+    // bind animate function so that it can call itself
     this.animate = this.animate.bind(this);
 
     this.scene = new THREE.Scene();
@@ -30,12 +33,33 @@ class ThreeRenderer {
     document.body.appendChild(this.renderer.domElement);
 
     // Create a this.camera, zoom it out from the model a bit, and add it to the this.scene.
-    this.camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 20000);
-    this.camera.position.set(0,6,0);
-    this.scene.add(this.camera);
+    this.camera = new THREE.PerspectiveCamera/*(45, WIDTH / HEIGHT, 0.1, 20000);*/( 100, window.innerWidth / window.innerHeight, 1, 1000000 );
+    this.camera.position.z = 30;
+    this.camera.position.y = 30;
+
+    console.log(this.renderer);
+
+    // Create effect composer
+    let composer = new THREE.EffectComposer(this.renderer);
+    composer.addPass(new THREE.RenderPass(this.scene, this.camera) );
+
+    let effect = new THREE.ShaderPass(getDistortionShaderDefinition());
+    composer.addPass(effect);
+    effect.renderToScreen = true;
+
+    // Setup distortion effect
+    var height = Math.tan(THREE.Math.degToRad(horizontalFOV) / 2) / this.camera.aspect;
+
+    this.camera.fov = Math.atan(height) * 2 * 180 / 3.1415926535;
+    this.camera.updateProjectionMatrix();
+
+    effect.uniforms["strength"].value = strength;
+    effect.uniforms["height"].value = height;
+    effect.uniforms["aspectRatio"].value = this.camera.aspect;
+    effect.uniforms["cylindricalRatio"].value = cylindricalRatio;
 
     // Set the background color of the this.scene.
-    this.renderer.setClearColorHex(0x333F47, 1);
+    this.renderer.setClearColor(0x333F47, 1);
 
     // Create a light, set its position, and add it to the this.scene.
     var light = new THREE.PointLight(0xffffff);
@@ -44,13 +68,14 @@ class ThreeRenderer {
 
     // Load in the mesh and add it to the this.scene.
     var loader = new THREE.JSONLoader();
-    loader.load( "models/treehouse_logo.js", (geometry) => {
+    loader.load( "models/m4.js", (geometry) => {
       var material = new THREE.MeshLambertMaterial({color: 0x999999});
       let mesh = new THREE.Mesh(geometry, material);
       this.scene.add(mesh);
     });
 
     // Add OrbitControls so that we can pan around with the mouse.
+    this.scene.add(this.camera);
     this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
   }
 
@@ -63,8 +88,6 @@ class ThreeRenderer {
 
   // Renders the this.scene and updates the render as needed.
   animate() {
-    // Huzzah for functional programming.
-    // Note that animate's context is bound to this in the constructor
     requestAnimationFrame(this.animate);
     
     // Render the this.scene.
